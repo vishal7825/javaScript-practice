1db7d9539ef8731c7b50af8185ac5605f1011311
vishal Bavaliya <vishalbavaliya7825@gmail.com>
2025-01-10 12:42:49 +0000

    practice

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,55 @@
+let score = "vishal"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true =>; false => 0;
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "vishal" => true
+
+
+let someNumber = 33
+
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+// *********************** OPerations *************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3)
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Vishal"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
