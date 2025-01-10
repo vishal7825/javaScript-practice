03ccd7f5b90000d5c0c780e4d6b584ee075e1bf8
vishal Bavaliya <vishalbavaliya7825@gmail.com>
2025-01-10 11:59:27 +0000

    practice

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,22 @@
+const accountId = 144453
+let accountEmail = "vishalbavaliya7825@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jasdan"
+
+// accountId = 2 // Not Allowed 
+
+accountEmail = "vishal@gmail.com"
+accountPassword = "1154545"
+accountCity = "Rajkot"
+let accountState;
+
+console.log(accountId)
+
+/*
+
+prefer Not to use var 
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as newer version
+
+console.log("Vishal")
+
+let name = "Vishal"
+let age = 18
+let isLoggedIn = true 
+
+// number => 2 to power 53
+// bigitn
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
